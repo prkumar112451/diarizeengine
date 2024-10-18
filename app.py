b233bbbe8beadf014d501bc78390dea2b2bc9702
prkumar112451 <38156723+prkumar112451@users.noreply.github.com>
2024-10-18 18:29:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,40 +21,91 @@ os.environ['TORCH_USE_CUDA_DSA'] = '1'
 app = FastAPI()
 
 # Configure logging
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S',
-)
-
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 logger = logging.getLogger(__name__)
 
 device = "cuda"
 batch_size = 8
 compute_type = "int8"
-YOUR_HF_TOKEN = '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'
-asr_options = {
-    "suppress_numerals": True  # Set suppress_numerals to True here
-}
-
+asr_options = {"suppress_numerals": True}
 language_in_use = "en"
+
+# Load models
 model = whisperx.load_model("small", device, compute_type=compute_type, asr_options=asr_options, language=language_in_use)
 model_a, metadata = whisperx.load_align_model(language_code=language_in_use, device=device)
-diarize_model = whisperx.DiarizationPipeline(use_auth_token=YOUR_HF_TOKEN, device="cuda:0")
+diarize_model = whisperx.DiarizationPipeline(use_auth_token=YOUR_HF_TOKEN, device=device)
 
 # Configuration for thread pool
-max_concurrent_tasks = 1  # You can change this as needed
-executor = concurrent.futures.ThreadPoolExecutor(max_workers=max_concurrent_tasks)
+max_concurrent_tasks = 1
+executor = ThreadPoolExecutor(max_workers=max_concurrent_tasks)
 task_queue = Queue()
-SECRET_KEY = 'your_secret_key_here'
+
+def is_stereo(wav_file_path: str) -> bool:
+    try:
+        command = ['ffprobe', '-i', wav_file_path, '-show_streams', '-select_streams', 'a:0']
+        process = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        stream_info = process.stderr.splitlines()
+        for line in stream_info:
+            if 'Audio:' in line:
+                return 'stereo' in line
+    except subprocess.SubprocessError as e:
+        logger.error("Failed to determine if audio is stereo: %s", e)
+    return False
+
+def split_stereo(wav_file_path: str):
+    base_filename, extension = os.path.splitext(os.path.basename(wav_file_path))
+    left_output = f'/tmp/{base_filename}_left{extension}'
+    right_output = f'/tmp/{base_filename}_right{extension}'
+    command = [
+        'ffmpeg', '-i', wav_file_path, '-filter_complex',
+        '[0:a]channelsplit=channel_layout=stereo[left][right]',
+        '-map', '[left]', left_output, '-map', '[right]', right_output
+    ]
+    subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    return left_output, right_output
+
+def assign_speaker(words, speaker_id):
+    for word in words:
+        word['speaker'] = speaker_id
+
+def merge_words_to_text(data):
+    result, current_text, current_words, current_speaker = [], "", [], None
+    for word in data['words']:
+        if not current_words or word['speaker'] == current_speaker:
+            current_text += word['word'] + " "
+            current_words.append(word)
+            current_speaker = word['speaker']
+        else:
+            result.append({'Text': current_text.strip(), 'Speaker': current_speaker, 'words': current_words})
+            current_text = word['word'] + " "
+            current_words = [word]
+    if current_words:
+        result.append({'Text': current_text.strip(), 'Speaker': current_speaker, 'words': current_words})
+    return result
+
+def merge_segments(output1, output2):
+    words1 = [word for segment in output1['segments'] for word in segment['words']]
+    words2 = [word for segment in output2['segments'] for word in segment['words']]
+    assign_speaker(words1, "SPEAKER_00")
+    assign_speaker(words2, "SPEAKER_01")
+    merged_words = words1 + words2
+    merged_segments = {'words': sorted(merged_words, key=lambda x: x['start'])}
+    return merged_segments
+
+def process_transcription(audio_path: str):
+    audio_data = whisperx.load_audio(audio_path)
+    result = model.transcribe(audio_data, batch_size=batch_size)
+    return whisperx.align(result["segments"], model_a, metadata, audio_data, device, return_char_alignments=False)
 
 def get_gpu_metrics():
     try:
-        result = subprocess.run(['nvidia-smi', '--query-gpu=utilization.gpu,temperature.gpu,memory.used,memory.total', '--format=csv,noheader,nounits'], capture_output=True, text=True)
+        result = subprocess.run(
+            ['nvidia-smi', '--query-gpu=utilization.gpu,temperature.gpu,memory.used,memory.total', '--format=csv,noheader,nounits'],
+            capture_output=True, text=True
+        )
         if result.returncode == 0:
-            output_lines = result.stdout.strip().split('\n')
             gpu_data = []
-            for line in output_lines:
+            for line in result.stdout.strip().split('\n'):
                 metrics = line.split(',')
                 gpu_data.append({
                     'Utilisation': float(metrics[0]),
@@ -63,160 +114,126 @@ def get_gpu_metrics():
                     'MemoryTotal': int(metrics[3])
                 })
             return gpu_data
-        else:
-            logger.info(f"Error running nvidia-smi: {result.stderr}")
+        logger.error(f"nvidia-smi error: {result.stderr}")
     except Exception as e:
-        logger.error("Error getting gpu: %s", e)
-        
-# Define transcribe_audio_worker function
-def transcribe_audio_worker(temp_audio_path, request_id, webhook_url, mask, language_code):
+        logger.error("Error fetching GPU metrics: %s", e)
+
+def transcribe_audio_worker(audio_path, request_id, webhook_url, mask, language_code, use_diarization_model):
     global model, model_a, metadata, diarize_model, language_in_use
 
-    if language_code != language_in_use:
-        del model
-        torch.cuda.empty_cache()
-        gc.collect()
-        
-        model = whisperx.load_model("small", device, compute_type=compute_type, asr_options=asr_options, language=language_code)
-        language_in_use = language_code
-
     try:
-        result_return = {}
-        # Diarize segments
-        audio_data = whisperx.load_audio(temp_audio_path)
+        logger.info("Started processing for request %s", request_id)
 
-        logger.info("about to start tasks %s for file %s", datetime.utcnow(), request_id)
+        # Transcription logic
+        final_result = {}
 
-        # Perform transcription        
-        result = model.transcribe(audio_data, batch_size=batch_size)
+        if use_diarization_model and is_stereo(audio_path):
+            logger.info("Processing stereo audio for request %s", request_id)
+            left_path, right_path = split_stereo(audio_path)
+            output_left = process_transcription(left_path)
+            output_right = process_transcription(right_path)
+            merged_segments = merge_segments(output_left, output_right)
+            final_result = {'segments': merge_words_to_text(merged_segments)}
+            os.remove(left_path)
+            os.remove(right_path)
 
-        # Perform alignment only for english
-        if language_in_use == 'en':
-            result_transcribe = whisperx.align(result["segments"], model_a, metadata, audio_data, device, return_char_alignments=False)
         else:
-            result_transcribe = result
+            if language_code != language_in_use:
+                model = whisperx.load_model("small", device, compute_type=compute_type, asr_options=asr_options, language=language_code)
+                language_in_use = language_code
             
-        # Perform diarization
-        diarize_result = diarize_model(audio_data)
+            audio_data = whisperx.load_audio(audio_path)
+            result = model.transcribe(audio_data, batch_size=batch_size)
 
-        logger.info("both tasks completed at %s for file %s", datetime.utcnow(), request_id)
+            if language_in_use == 'en':
+                result_transcribe = whisperx.align(result["segments"], model_a, metadata, audio_data, device, return_char_alignments=False)
+            else:
+                result_transcribe = result
 
-        # Assign word speakers
-        final_result = whisperx.assign_word_speakers(diarize_result, result_transcribe)
+            diarize_result = diarize_model(audio_data)
+            final_result = whisperx.assign_word_speakers(diarize_result, result_transcribe)
+            # Clear audio_data from memory
+            del audio_data
 
-        # Process transcript for response
-        logger.info("result ready to send at %s for file %s", datetime.utcnow(), request_id)
-        if mask:
-            try:
+        try : 
+            if mask:
                 mask_transcript(final_result['segments'])
-            except Exception as e:
-                logger.error("Error masking transcript: %s", e)
-                traceback.print_exc()
-            
-        result_return['transcription'] = final_result['segments']
-        result_return['requestID'] = request_id
-
-        # Post-process to fill missing word attributes and assign speakers        
-        try:
-            for segment in result_return["transcription"]:
-                # Assign SPEAKER_00 if no speaker is assigned at the segment level
-                if "speaker" not in segment:
-                    segment["speaker"] = "SPEAKER_00"
-                
-                previous_word = None
-                for word in segment["words"]:
-                    # Assign speaker from segment level if word level speaker is missing
-                    if "speaker" not in word:
-                        word["speaker"] = segment["speaker"]
-    
-                    # Fill missing word attributes
-                    if "start" not in word or "end" not in word or "score" not in word:
-                        if previous_word:
-                            word["start"] = word["end"] = previous_word["end"]
-                            word["score"] = 0.99
-                        else:
-                            # If no previous word, use the next word's start and speaker
-                            next_word = next((w for w in segment["words"] if "start" in w and "end" in w), None)
-                            if next_word:
-                                word["start"] = word["end"] = next_word["start"]
-                                word["score"] = 0.99
-                    previous_word = word
         except Exception as e:
-            logger.error("Error processing transcript: %s", e)
-            traceback.print_exc()
+            logger.error("Error processing transcript attributes: %s", e)
+        
+        result_return = {'transcription': final_result['segments'], 'requestID': request_id}
+        process_transcription_attributes(result_return)
+        
+        if webhook_url:
+            requests.post(webhook_url, json=result_return)
 
-        # Save result to a text file
         result_file_path = f'/tmp/{request_id}.txt'
         with open(result_file_path, 'w') as result_file:
             json.dump(result_return, result_file)
 
-        logger.info("Processing completed for request %s", request_id)
-        if webhook_url is not None:
-            requests.post(webhook_url, json=result_return)
+        logger.info("Completed processing for request %s", request_id)
+        
     except Exception as e:
-        logger.error("Error processing audio: %s", e)
-        traceback.print_exc()
+        logger.error("Error processing audio for request %s: %s", request_id, e)
+        raise HTTPException(status_code=500, detail=f"Error processing audio: {str(e)}")
     finally:
-        # Mark task as done in the queue
-        task_queue.get()
+        if os.path.exists(audio_path):
+            os.remove(audio_path)  # Cleanup original audio file
+        task_queue.get()  # Mark task as done
         task_queue.task_done()
 
-        # Clear memory
-        del audio_data
-        del result
-        del result_transcribe
-        del diarize_result
-        del final_result
-        torch.cuda.empty_cache()
-        gc.collect()
-
+def process_transcription_attributes(result_return):
+    try:
+        for segment in result_return["transcription"]:
+            if "speaker" not in segment:
+                segment["speaker"] = "SPEAKER_00"
+            previous_word = None
+            for word in segment["words"]:
+                if "speaker" not in word:
+                    word["speaker"] = segment["speaker"]
+                if "start" not in word or "end" not in word or "score" not in word:
+                    if previous_word:
+                        word.update({"start": previous_word["end"], "end": previous_word["end"], "score": 0.99})
+                    else:
+                        next_word = next((w for w in segment["words"] if "start" in w and "end" in w), None)
+                        if next_word:
+                            word.update({"start": next_word["start"], "end": next_word["start"], "score": 0.99})
+                previous_word = word
+    except Exception as e:
+        logger.error("Error processing transcript attributes: %s", e)
 
 @app.get('/gpu/metrics')
 def get_gpu_metrics_route():
-    gpu_metrics = get_gpu_metrics()
-    return gpu_metrics
+    return get_gpu_metrics()
 
-# Protected endpoint - requires authentication
 @app.post('/transcribe')
 async def transcribe_audio(audio: UploadFile = File(...), 
                            webhook_url: Optional[str] = Form(None),
                            mask: Optional[bool] = Form(False),
-                           language_code: Optional[str] = Form("en")):
+                           language_code: Optional[str] = Form("en"),
+                           use_diarization_model: Optional[bool] = Form(False),
+                           no_of_participants: Optional[int] = Form(2)):
     try:
-        logger.info("Processing root request at %s for file %s", datetime.utcnow(), audio.filename)
-        logger.info(f"Received webhook_url: {webhook_url}")
+        logger.info("Received file %s for transcription", audio.filename)
         request_id = str(uuid.uuid4())
-        # Save audio file temporarily
-        temp_audio_path = f'/tmp/{audio.filename}'
+        temp_audio_path = f'/tmp/{request_id}_{audio.filename}'
         with open(temp_audio_path, 'wb') as f:
-            content = await audio.read()
-            f.write(content)
+            f.write(await audio.read())
 
-        logger.info("temp file created at %s for file %s", datetime.utcnow(), audio.filename)
+        logger.info("Temp file created at %s for file %s", datetime.utcnow(), audio.filename)
 
-        # Log the current queue size before submitting the task
-        queue_size_before = task_queue.qsize()
-        logger.info(f"Queue size before adding the task: {queue_size_before}")
-        
-        # Put task in the queue and submit to the thread pool
+        # Submit task to executor
         task_queue.put(request_id)
 
-        # Submit the task to the thread pool
-        task_args = (temp_audio_path, request_id, webhook_url, mask, language_code)
+        task_args = (temp_audio_path, request_id, webhook_url, mask, language_code, use_diarization_model)
         executor.submit(transcribe_audio_worker, *task_args)
 
-        # Log the current queue size after submitting the task
-        queue_size_after = task_queue.qsize()
-        logger.info(f"Queue size after adding the task: {queue_size_after}")
-        
-        # Return a response immediately, while the background task processes the audio
-        return {"message": "Audio processing started, result will be sent to webhook URL", "requestID": request_id}
+        return {"message": "Audio processing started, result will be sent to webhook URL if provided", "requestID": request_id}
 
     except Exception as e:
-        traceback.print_exc()
-        raise HTTPException(status_code=500, detail=f"Error processing audio: {str(e)}")
+        logger.error("Error receiving audio file: %s", e)
+        raise HTTPException(status_code=500, detail=f"Error receiving audio: {str(e)}")
 
-# Endpoint to update batch size without authentication
 @app.post('/update_batch_size')
 async def update_batch_size(new_batch_size: int):
     global batch_size
@@ -224,16 +241,13 @@ async def update_batch_size(new_batch_size: int):
     logger.info(f"Batch size updated to {batch_size}")
     return {"message": f"Batch size updated to {batch_size}"}
 
-# Endpoint to update concurrent tasks without authentication
 @app.post('/update_concurrent_tasks')
 async def update_concurrent_tasks(new_max_concurrent_tasks: int):
-    global max_concurrent_tasks
-    max_concurrent_tasks = new_max_concurrent_tasks
     global executor
     executor.shutdown(wait=False)
-    executor = concurrent.futures.ThreadPoolExecutor(max_workers=max_concurrent_tasks)
-    logger.info(f"Max concurrent tasks updated to {max_concurrent_tasks}")
-    return {"message": f"Max concurrent tasks updated to {max_concurrent_tasks}"}
+    executor = ThreadPoolExecutor(max_workers=new_max_concurrent_tasks)
+    logger.info(f"Max concurrent tasks updated to {new_max_concurrent_tasks}")
+    return {"message": f"Max concurrent tasks updated to {new_max_concurrent_tasks}"}
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0")
+    uvicorn.run(app, host="0.0.0.0", port=8000)
